a4188170a9511d5ccc4d714cf005062cb1dcf4b4
Jayanth <vagrant@nowhere.com>
2015-09-28 19:32:55 -0400

    Python and SQLite work
    sample application in python and sqlite

---
+++ Python & sqlite/helloWorld.py
@@ -0,0 +1,14 @@
+from bottle import route, run, template
+
+@route('/hi')
+@route('/hello')
+def hello():
+    return "Hello World!"
+
+@route('/')
+@route('/hello/<name>')
+def greet(name='Stranger'):
+    return template('Hello {{name}}, how are you?', name=name)
+
+
+run(host='localhost', port=8080, debug=True)
